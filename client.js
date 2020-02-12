ae1371bbc43a04d3be3bb2897b3e4e9b0d947d84
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-12 19:22:11 +0000

    changed to settime interval

--- client.js
+++ client.js
@@ -8,18 +8,39 @@ const connect = function() {
     host: "172.46.0.236",
     port: 50541
   });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data)
+  })
+   
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
-  conn.on('connect', () => {
-    console.log("Successfully connected to game server");
-    conn.write("Name: SJL")
+  conn.on('connect', () => {  //connect to the server and do below
+    console.log("Successfully connected to game server"); // says you're connected on terminal
+    conn.write("Name: SJL") // your initials are on the game
+   
   })
+    
+
+    conn.on('connect', () => {
+
+      setInterval(() => {
+        conn.write("Move: up") // print out the words 
+      }, 50)
+    //add 1 second each time it loops again
+    })
+    
+
 
   return conn;
   }
 
 
+//   "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
 // connect();
 
 
